b2bf3fb6ce3508218a2a7b0cf7657c34f367ef4f
吕岩 <1258321826@qq.com>
2025-06-06 21:28:54 +0800

    初始化iniit

---
+++ common/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+type Config struct {
+	Mysql
+	Redis
+	Server
+	Elastic
+	AliyunGpt
+}
+
+type Server struct {
+	Host string `yaml:"Host"`
+	Port int    `yaml:"Port"`
+}
+
+type Elastic struct {
+	Host string `yaml:"Host"`
+	Port int    `yaml:"Port"`
+	User string `yaml:"User"`
+	Pass string `yaml:"Pass"`
+}
+
+type Mysql struct {
+	Host     string `yaml:"Host"`
+	Port     int    `yaml:"Port"`
+	UserName string `yaml:"UserName"`
+	Password string `yaml:"PassWord"`
+	Database string `yaml:"DataBase"`
+}
+
+type Redis struct {
+	Host     string `yaml:"Host"`
+	Port     string `yaml:"Prot"`
+	User     string `yaml:"User"`
+	Password string `yaml:"PassWord"`
+	DB       int    `yaml:"DB"`
+}
+
+type AliyunGpt struct {
+	ApiKey string `yaml:"ApiKey"`
+}
