783d9e06d109a88fca09b5b9b86d3f53e84bc964
吕岩 <1258321826@qq.com>
2025-06-07 09:55:45 +0800

    mysql初始化

--- server/inits/mysql/init.go
+++ server/inits/mysql/init.go
@@ -1,1 +1,24 @@
 package mysql
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"server/global"
+)
+
+func InitMysql() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		global.CONFIG.Mysql.User,
+		global.CONFIG.Mysql.Pass,
+		global.CONFIG.Mysql.Host,
+		global.CONFIG.Mysql.Port,
+		global.CONFIG.Mysql.DB,
+	)
+	var err error
+	global.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("链接mysql出错.error:", err)
+	}
+	fmt.Println("mysql链接成功.")
+}
